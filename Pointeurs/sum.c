36dacaf588c44bd68ad8bf38b048630055f874f0
VM IUT <vm@iut>
2018-06-01 10:27:13 +0200

    seance2 fonction sum finie

--- Pointeurs/sum.c
+++ Pointeurs/sum.c
@@ -1,31 +1,28 @@
 #include <stdio.h>
 
 double sum1(double *tab, int size) {
-	printf(" [ "); 
+	double somme=0;
 	for(int i=0;i<size;++i){
-		printf("%f ",tab[i]);
+		somme=somme+tab[i];
 	}
-	printf("]\n");
+	return somme;
 }
 
 double sum2(double *tab, int size) {
-	printf(" [ "); 
+	double somme=0;
 	double *fin=tab+size; 
 	for(double *p=tab;p<fin;++p){
-		printf("%f ",*p);
+		somme=somme+*p;
 	}
-	printf("]\n");
+return somme;
 }
 
 int main(void) {
-	float tab[5];
-	tab[0]=100;
-	tab[1]=10;
-	tab[2]=1;
-	tab[3]=0.1;
-	tab[4]=0.01;
-	
-	sum1(tab[],5);
-	sum2(tab[],5);
+	double tab[5]={100,10,1,0.1,0.01};
+
+	double somme1=sum1(tab,5);
+double somme2=sum2(tab,5);
+	printf("Resultat fonction 1=%f",somme1);
+	printf("Resultat fonction 2=%f",somme2);
 
 }
